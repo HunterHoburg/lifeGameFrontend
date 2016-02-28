7bdfff1ec9c72a8459be5fd1c713a97d8d0cd868
hhoburg <hhoburg@gmail.com>
2016-02-27 18:49:13 -0700

    made some stylistic changes and got closer to implementing function to add unique players and guests to the games_players table

--- js/newGame.controller.js
+++ js/newGame.controller.js
@@ -16,24 +16,19 @@ function NewGameController (signinService, signupService, guestSigninService) {
   vm.guestCounter = 1;
   vm.colorPick = function(color) {
     vm.chosenColor = color;
-    console.log(vm.chosenColor);
-  }
+  };
   function signin (emailIn, passwordIn) {
     // playerInfo should be: {email: '', password: ''}
     var playerInfo = {email: emailIn, password: passwordIn};
-    console.log(playerInfo);
     signinService(playerInfo)
       .then(function(playerData){
         if (playerData.data.email === playerInfo.email) {
-          // console.log(playerData.data.id);
           // add returned player to players obj
           playerData.data.color = vm.chosenColor;
           playerData.data.player_id = vm.playerId;
-          // console.log(playerData.data);
           vm.players.push(playerData.data);
           vm.emailInput = '';
           vm.passwordInput = '';
-          // console.log(vm.players);
         } else {
           vm.errorMessage = 'wrong username or password';
         }
@@ -50,7 +45,6 @@ function NewGameController (signinService, signupService, guestSigninService) {
     guest.name = 'Guest ' + vm.guestCounter;
     guestSigninService(guest)
       .then(function(guest){
-        console.log(guest.data.message[0]);
       });
       vm.players.push(guest);
       vm.chosenColor = '';
